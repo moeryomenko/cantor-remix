c7c2b6fa2cc746a94e2cf232b57bb50687eb6957
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2025-04-04 12:36:13 +0000

    feat: add equals keymap combo
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- features/combo.c
+++ features/combo.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM cmb_minus[] = {LG(KC_F), RG(KC_J), COMBO_END};
+const uint16_t PROGMEM cmb_equals[] = {RG(KC_J), RS(KC_K), COMBO_END};
 const uint16_t PROGMEM cmb_quote[] = {LS(KC_D), RS(KC_K), COMBO_END};
 const uint16_t PROGMEM cmb_plus[] = {KC_E, KC_R, COMBO_END};
 const uint16_t PROGMEM cmb_tmux[] = {R(KC_ENTER), R(KC_ESC), COMBO_END};
@@ -8,6 +9,7 @@ const uint16_t PROGMEM cmb_bkspace[] = {KC_U, KC_I, COMBO_END};
 
 enum combos {
     MINUS,
+    EQUALS,
     QUOTE,
     PLUS,
     TMUX,
@@ -16,6 +18,7 @@ enum combos {
 
 combo_t key_combos[] = {
     [MINUS] = COMBO(cmb_minus, KC_MINUS),     // f j
+    [EQUALS] = COMBO(cmb_equals, KC_EQL),     // j k
     [QUOTE] = COMBO(cmb_quote, KC_QUOTE),     // d k
     [PLUS] = COMBO(cmb_plus, KC_KP_PLUS),     // e r
     [TMUX] = COMBO(cmb_tmux, LCTL(KC_B)),     // Ent Spc
